1f5fa60cbb50668392130d2ea58725fc565294ac
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-11-17 23:37:15 -0800

    finally got it work

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/WorkingDirectory/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/WorkingDirectory/ann.py
@@ -1,4 +1,15 @@
-# Classification template
+# Artificial Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
 
 # Importing the libraries
 import numpy as np
@@ -10,15 +21,15 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1  = LabelEncoder()
+labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2  = LabelEncoder()
+labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
-X=X[:,1:]
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -30,22 +41,34 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting classifier to the Training set
-# Create your classifier here
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
+# Initialising the ANN
 classifier = Sequential()
-classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))
-classifier.add(Dense(activation="relu", units=6, kernel_initializer="uniform"))
-classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="uniform"))
 
-#this line doesn't work
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dropout(p=0.1))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(p=0.1))
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dropout(p=0.1))
+# Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-#wth is batch_size?
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -54,3 +77,106 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+from keras.wrappers.scikit_learn  import KerasClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier 
+
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = 1)
+
+mean = accuracies.mean()
+variance = accuracies.std()
+
+
+def build_classifier_2(op):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = op, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier 
+classifier = KerasClassifier(build_fn = build_classifier_2)
+parameters = {'batch_size':[10,25],
+              'epochs': [500, 800], 
+              'op':['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10).fit(X_train, y_train)
+
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+
+
+
+
+
+
+
+
+
+
+#Solution = (classifier.predict(sc.transform(np.array([[0,0,600,1, 40,3, 60000,2, 1, 1, 50000]])))>0.5)
+
+comment = '''
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_transformer
+
+onehotencoder = make_column_transformer(
+    ([0, 3,4,5,6,7,8,9], StandardScaler()),
+    ([1,2], OneHotEncoder())
+)
+
+X = onehotencoder.fit_transform(X)
+X=X[:,[0,1,2,3,4,5,6,7,9,10,11]]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting classifier to the Training set
+# Create your classifier here
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))
+classifier.add(Dense(activation="relu", units=6, kernel_initializer="uniform"))
+classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="uniform"))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#wth is batch_size?
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+Solution = classifier.predict(onehotencoder.fit_transform(np.array([[600, 40,3, 60000,2, 1, 1, 50000,0,0,0]])))
+'''
