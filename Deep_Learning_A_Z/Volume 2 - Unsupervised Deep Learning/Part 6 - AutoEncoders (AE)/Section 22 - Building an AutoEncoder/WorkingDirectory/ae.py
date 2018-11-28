f06bf17c75900f09be47f4c15860c137a4265da1
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-11-28 04:29:44 -0800

    AE done, but not successful. Need to review more materials

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/WorkingDirectory/ae.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/WorkingDirectory/ae.py
@@ -46,11 +46,71 @@ test_set = torch.FloatTensor(test_set)
 class SAE(nn.Module):
     def __init__(self ):
         super(SAE, self).__init__()
-        self.fc1 = nn.Linear(in_features=nb_movies, out_features = 20)
+        self.fc1 = nn.Linear(in_features=nb_movies, out_features = 20)#20 comes from instrucotr's experiment(s)
         self.fc2 = nn.Linear(20,10)
         self.fc3 = nn.Linear(10, 20)
         self.fc4 = nn.Linear(20, nb_movies)
         self.activation = nn.Sigmoid()
         
-    def forward(self, x):
-        self.activation = 
+    def forward(self, input_x):
+        input_x = self.activation(self.fc1(input_x))
+        input_x = self.activation(self.fc2(input_x))
+        input_x = self.activation(self.fc3(input_x))
+        input_x = self.fc4(input_x)
+        return input_x
+    
+    
+sae = SAE()
+criterion = nn.MSELoss()
+optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5) # "lr" and "weight_decay" come from instrucotr's experiment(s)
+
+nb_epoch = 200
+for epoch in range(nb_epoch):
+    train_loss = 0
+    users_rated_at_least_1 = 0.
+    
+    for id_user in range(nb_users):
+        input_set = Variable(training_set[id_user]).unsqueeze(0) #make "single value" to ["single value"] (vector size == 1)
+        target = input_set.clone()
+        
+        if torch.sum(target.data > 0) > 0:
+            output = sae.forward(input_set)
+            target.require_grad = False #wtf???
+            output[target == 0] = 0
+            loss = criterion(output, input_set)
+            mean_corrector = nb_movies/float(torch.sum(target.data>0)+1e-10) #"1e-10" is system required, to avoid the denuminator is not 0
+            loss.backward() #decide direction (increase/decrease)
+            train_loss += np.sqrt(loss.data[0]*mean_corrector)
+            users_rated_at_least_1+=1.
+            optimizer.step() #decide intensity?
+            
+
+    print('epoch: '+str(epoch+1)+' loss: '+str(train_loss/users_rated_at_least_1))    
+    
+test_loss = 0
+users_rated_at_least_1 = 0.
+    
+for id_user in range(nb_users):
+    input_set = Variable(training_set[id_user]).unsqueeze(0) #make "single value" to ["single value"] (vector size == 1)
+    #target = Variable(test_set[id_user])
+    target = Variable(test_set[id_user]).unsqueeze(0) #make "single value" to ["single value"] (vector size == 1)
+                                                      #not accurate!!!
+        
+    if torch.sum(target.data > 0) > 0:
+        output = sae(input_set)
+        target.require_grad = False #wtf???
+        output[target == 0] = 0
+        loss = criterion(output, input_set)
+        mean_corrector = nb_movies/float(torch.sum(target.data>0)+1e-10) #"1e-10" is system required, to avoid the denuminator is not 0
+            
+        test_loss += np.sqrt(loss.data[0]*mean_corrector)
+        users_rated_at_least_1+=1.
+            
+
+print('test loss: '+str(test_loss/users_rated_at_least_1))    
+    
+
+            
+            
+            
+        
