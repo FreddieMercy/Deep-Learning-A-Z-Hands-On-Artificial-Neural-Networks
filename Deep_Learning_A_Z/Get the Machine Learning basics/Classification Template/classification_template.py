40a1d85683f0145bb0b50e2139c20ade6ddb501b
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-11-28 16:43:11 -0800

    classification done

--- Deep_Learning_A_Z/Get the Machine Learning basics/Classification Template/classification_template.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Classification Template/classification_template.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -23,12 +23,18 @@ X_test = sc.transform(X_test)
 # Fitting classifier to the Training set
 # Create your classifier here
 
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(random_state = 0)
+
+classifier.fit(X_train, y_train)
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+
+cm = confusion_matrix(y_true = y_test, y_pred = y_pred)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
