2ba139263c3fe4b4d9073b92713ad3401e7e622b
FreddieMercy <junhao.zhang.freddie@gmail.com>
2018-11-28 16:00:56 -0800

    Logistic Regression done, but it is much alike ann

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -9,3 +9,28 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import Imputer #"imputer" adds missing values to the rows
+
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis=0)
+
+X[:, 1:3] = imputer.fit(X[:, 1:3]).transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:,1:]
+labelencoder_Y = LabelEncoder()
+y = labelencoder_Y.fit_transform(y)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X  = sc.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
