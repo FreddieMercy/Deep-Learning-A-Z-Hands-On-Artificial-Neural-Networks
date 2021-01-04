05af449b9177d40424f269d73b5a76de895ce182
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-03 16:33:29 -0800

    sync code

--- Practice/Practice/main.py
+++ Practice/Practice/main.py
@@ -7,3 +7,15 @@ df_txt = pd.read_csv('data/pokemon_data/pokemon_data.txt', delimiter='\t')
 print(df_txt.head(10))
 
 print(df.tail(10))
+df.columns
+print(df['Name'])
+print(df.Name)
+print(df.iloc[1:4])  # stands for "index location". Contrary to default "label location"
+
+print(df.loc[1:4])
+print(df.loc[1:4, 'Name'])
+
+df.iloc[2, 1]
+
+for index, row in df.iterrows():
+    print(index, row)
