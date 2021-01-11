e65092bf61a105bbd569498c671d531a35768556
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-10 21:28:25 -0800

    Sklearn Practice

--- Practice/main.py
+++ Practice/main.py
@@ -1,12 +1,22 @@
-from src.MatplotlibPractice import matplotlibPractice
-from src.NumpyPractice import numpyPractice
-from src.PandasPractice import pandasPractice
+from sklearn import datasets, neighbors, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
 
-def main():
-    numpyPractice()
-    matplotlibPractice()
-    pandasPractice()
+print(x.shape)
+print(y.shape)
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)  # so train size -> 1 - 0.2 = 0.8?
 
-main()
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(x_test.shape)
+
+data = pd.read_csv('data/data.txt', delimiter='\t')
+
+print(data)
