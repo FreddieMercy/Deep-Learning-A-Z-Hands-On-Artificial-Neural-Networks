543c5e9c6b656606dd9f3bf30a6ff2667b937b76
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-12 23:24:52 -0800

    add new Jupyter Notebook

--- Practice/main.py
+++ Practice/main.py
@@ -1,7 +1,14 @@
 from sklearn import svm  # S.V. Machine (Support Vector Machine)
+import numpy as np
 
-data = [[0, 0], [1, 1]]
-target = [0, 1]
+data = np.array([np.arange(0, 9), np.arange(0, 9)])
+data = data.T
+target = np.arange(0, 9)
+
+print(data)
+print(target)
 
 clf = svm.SVC()  # why no alpha?
 clf.fit(data, target)
+
+print(clf.predict([[100000, -100]]))
