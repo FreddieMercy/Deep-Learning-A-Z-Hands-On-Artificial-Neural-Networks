3da9d10a3354a5e7c5615507c7422cc331274919
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-12 23:07:37 -0800

    Move Jupyter Notebook files to one folder

--- Practice/main.py
+++ Practice/main.py
@@ -1,14 +1,7 @@
-from src.MatplotlibPractice import matplotlibPractice
-from src.NumpyPractice import numpyPractice
-from src.PandasPractice import pandasPractice
-from src.SklearnPractice.LinearRegression import linearRegressionPractice
+from sklearn import svm  # S.V. Machine (Support Vector Machine)
 
+data = [[0, 0], [1, 1]]
+target = [0, 1]
 
-def main():
-    numpyPractice()
-    matplotlibPractice()
-    pandasPractice()
-    linearRegressionPractice()
-
-
-main()
+clf = svm.SVC()  # why no alpha?
+clf.fit(data, target)
