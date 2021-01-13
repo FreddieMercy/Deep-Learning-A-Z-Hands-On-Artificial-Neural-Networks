047f64a0a10b3c2e701169da53f5f8eeaaea7c76
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-12 20:04:18 -0800

    Sklearn Practice 1.1 (#4)
    * Sklearn Practice
    
    * Sklearn linear regression
    
    * linear regression (Practice) done

--- Practice/main.py
+++ Practice/main.py
@@ -1,12 +1,14 @@
 from src.MatplotlibPractice import matplotlibPractice
 from src.NumpyPractice import numpyPractice
 from src.PandasPractice import pandasPractice
+from src.SklearnPractice.LinearRegression import linearRegressionPractice
 
 
 def main():
     numpyPractice()
     matplotlibPractice()
     pandasPractice()
+    linearRegressionPractice()
 
 
 main()
