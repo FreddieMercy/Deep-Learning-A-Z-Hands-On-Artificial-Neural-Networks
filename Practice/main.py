028372cba5436ecb70303c19ac71ec1907578b1c
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-17 16:33:55 -0800

    ignore generated graph

--- Practice/main.py
+++ Practice/main.py
@@ -9,3 +9,10 @@ dt_clf.fit(data, target)
 print(dt_clf.predict([[100., -10.]]))
 print(dt_clf.predict_proba([[100., -10.]]))  # probability of being one of the $(target.length) classes
 tree.plot_tree(dt_clf)
+
+print(tree.export_text(dt_clf))
+
+import graphviz
+
+graph = graphviz.Source(tree.export_graphviz(dt_clf))  # 'Source' means 'import/load'?
+graph.render("tree_graph")
