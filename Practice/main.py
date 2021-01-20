d55c543e6be18462024710cd03c77be29f7ea03a
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-19 19:49:01 -0800

    add Ensemble practice

--- Practice/main.py
+++ Practice/main.py
@@ -2,6 +2,13 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, ExtraTre
 # ensemble means "vote"
 from sklearn.neighbors import KNeighborsClassifier
 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
+
+data_i, targeti = load_iris(return_X_y=True)
+data_train, data_test, target_train, target_test = train_test_split(data_i, targeti, random_state=42,
+                                                                    stratify=targeti, test_size=0.7)
+
 bagging = BaggingClassifier(max_samples=0.5,  # half rows
                             max_features=0.5,  # half columns
                             base_estimator=KNeighborsClassifier())
@@ -14,3 +21,15 @@ rndForest = RandomForestClassifier(n_estimators=10)
 exTree = ExtraTreesClassifier(max_samples=0.5,  # half rows
                               max_features=0.5,  # half columns
                               n_estimators=10)
+
+bagging.fit(data_train, target_train)
+rndForest.fit(data_train, target_train)
+exTree.fit(data_train, target_train)
+
+print(cross_val_score(bagging, data_test, target_test, cv=5).mean())
+print(cross_val_score(rndForest, data_test, target_test, cv=5).mean())
+print(cross_val_score(exTree, data_test, target_test, cv=5).mean())
+
+print(cross_val_predict(bagging, data_test, target_test, cv=5))
+print(cross_val_predict(rndForest, data_test, target_test, cv=5))
+print(cross_val_predict(exTree, data_test, target_test, cv=5))
