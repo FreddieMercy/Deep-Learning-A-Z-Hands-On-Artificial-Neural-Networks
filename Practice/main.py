56220968d9a478679b8b6d2e22f9fbd6ac95ed44
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-21 00:13:00 -0800

    restore main.py

---
+++ Practice/main.py
@@ -0,0 +1,20 @@
+from src.MatplotlibPractice import matplotlibPractice
+from src.NumpyPractice import numpyPractice
+from src.PandasPractice import pandasPractice
+from src.SklearnPractice.LinearRegression import linearRegressionPractice
+from src.SklearnPractice.SVC import svcPractice
+from src.SklearnPractice.NearestNeighbors import nearestNeighborsPractice
+from src.SklearnPractice.DecisionTree import decisionTreePractice
+
+
+def main():
+    numpyPractice()
+    matplotlibPractice()
+    pandasPractice()
+    linearRegressionPractice()
+    svcPractice()
+    nearestNeighborsPractice()
+    decisionTreePractice()
+
+
+main()
