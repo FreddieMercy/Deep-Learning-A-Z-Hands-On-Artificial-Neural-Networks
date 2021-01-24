3ef02861657c6851fda0634c906d97c12823ef03
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-24 14:24:14 -0800

    Other Practice

---
+++ Practice/src/SklearnPractice/Others.py
@@ -0,0 +1,15 @@
+def otherPractice():
+    from sklearn.preprocessing import LabelBinarizer
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    import numpy as np
+
+    zero_to_two = np.arange(0, 3)
+    lb = LabelBinarizer()
+    print(zero_to_two)
+
+    # tmp = [0,1,2]
+    # [1, 0 ,0] means first item in tmp (a.k.a: 0)
+    # [0, 1 ,0] means second item in tmp (a.k.a: 1)
+    # [0, 0 ,1] means third item in tmp (a.k.a: 2)
+    print(lb.fit_transform(zero_to_two))
