93e0a10bdc359456831000d912bc2ec46eab5bca
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-01-24 19:18:11 -0800

    SelectKBest

--- Practice/main.py
+++ Practice/main.py
@@ -1,27 +1,15 @@
-from src.MatplotlibPractice import matplotlibPractice
-from src.NumpyPractice import numpyPractice
-from src.PandasPractice import pandasPractice
-from src.SklearnPractice.LinearRegression import linearRegressionPractice
-from src.SklearnPractice.SVC import svcPractice
-from src.SklearnPractice.NearestNeighbors import nearestNeighborsPractice
-from src.SklearnPractice.DecisionTree import decisionTreePractice
-from src.SklearnPractice.Ensemble import ensemblePractice
-from src.SklearnPractice.Others import otherPractice
-from Comparison import Comparison
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import SelectKBest, \
+    chi2  # chi2 is a math formula: https://www.google.com/search?q=chi2&oq=chi2&aqs=chrome..69i57j0i10i457j0l2j0i10j0j0i10j0i395l3.261j1j9&sourceid=chrome&ie=UTF-8#wptab=s:H4sIAAAAAAAAAONgVuLSz9U3MDItNitJfsToyi3w8sc9YSmbSWtOXmM04-IKzsgvd80rySypFNLgYoOy5Lj4pJC0aTBI8XAh8Xl2MUm6ppQmJ5Zk5ucl5jjn5yWnFpS45RflluYkLmKVSM7I1C0uLE0sSk1RKEktLlFIg0gBAOCLz8-NAAAA
 
+X, Y = load_iris(return_X_y=True)
 
-def main():
-    numpyPractice()
-    matplotlibPractice()
-    pandasPractice()
-    linearRegressionPractice()
-    svcPractice()
-    nearestNeighborsPractice()
-    decisionTreePractice()
-    ensemblePractice()
-    otherPractice()
+kBest_1 = SelectKBest(chi2, k=1)
+kBest_2 = SelectKBest(chi2, k=2)
+kBest_3 = SelectKBest(chi2, k=3)
+kBest_4 = SelectKBest(chi2, k=4)
 
-
-main()
-print("--------------------------------------------------------------------")
-Comparison()
+print(kBest_1.fit_transform(X, Y))
+print(kBest_2.fit_transform(X, Y))
+print(kBest_3.fit_transform(X, Y))
+print(kBest_4.fit_transform(X, Y))
