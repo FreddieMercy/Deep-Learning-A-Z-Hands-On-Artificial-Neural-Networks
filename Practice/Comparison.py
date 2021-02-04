0d0c695a6501cce29e44ada77b675f3d2d6c0a8a
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-02-04 00:04:42 -0800

    Sklearn Practice - Part 1 (#9)
    * LabelBinarizer practice
    
    * jupyter notebook Multiclass
    
    * Other Practice
    
    * OutputCodeClassifier practice added
    
    * MultiOutputClassifier Practice added
    
    * MultiOutputRegressor pratice added
    
    * Sklean - Feature Reduce (#10)
    
    * add VarianceThreshold
    
    * SelectKBest
    
    * added SelectFromModel
    
    * practice done
    
    * Sklearn Practice - last part of Part 1 (#11)

--- Practice/Comparison.py
+++ Practice/Comparison.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, make_classification, make_multilabel_classification, load_linnerud
 from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier, ExtraTreesClassifier, \
     GradientBoostingClassifier, GradientBoostingRegressor, VotingClassifier, VotingRegressor, \
     StackingClassifier, StackingRegressor
@@ -8,6 +8,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn import neighbors, svm
 from sklearn.linear_model import LogisticRegression, BayesianRidge, LassoLars, LinearRegression
+from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier, OutputCodeClassifier
+from sklearn.multioutput import MultiOutputRegressor, MultiOutputClassifier
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 
 
 def Comparison():
@@ -101,6 +104,16 @@ def Comparison():
 
     sr.fit(data_train, target_train)
 
+    # OneVsRestClassifier and OneVsOneClassifier are decorator that decorates the 'estimator'
+    # mulSVC should be same to svc (svc by default is ovr), 但怎么打脸了，还不容易converge？
+    mulSVC = OneVsRestClassifier(estimator=svm.LinearSVC(random_state=0, max_iter=9000))
+    mulSVC.fit(data_train, target_train)
+
+    # 哦，看起来我之前错了：svc by default is ovo。
+    # oneSVC should be same to svc
+    oneSVC = OneVsOneClassifier(estimator=svm.LinearSVC(random_state=0, max_iter=9000))
+    oneSVC.fit(data_train, target_train)
+
     print(nca_knn.score(data_test, target_test))
     print(knn.score(data_test, target_test))
     print(nc.score(data_test, target_test))
@@ -118,3 +131,151 @@ def Comparison():
     print(vr.score(data_test, target_test))
     print(sc.score(data_test, target_test))
     print(sr.score(data_test, target_test))
+    print(mulSVC.score(data_test, target_test))
+    print(oneSVC.score(data_test, target_test))
+
+    # Not a lot of improvement with greater 'hidden_layer_sizes' ...
+    mlpc100 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(100,))
+    mlpc200 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(200,))
+    mlpc300 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(300,))
+    mlpc400 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(400,))
+    mlpc500 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(500,))
+    mlpc600 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(600,))
+    mlpc700 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(700,))
+    mlpc800 = MLPClassifier(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(800,))
+
+    mlpc100.fit(data_train, target_train)
+    mlpc200.fit(data_train, target_train)
+    mlpc300.fit(data_train, target_train)
+    mlpc400.fit(data_train, target_train)
+    mlpc500.fit(data_train, target_train)
+    mlpc600.fit(data_train, target_train)
+    mlpc700.fit(data_train, target_train)
+    mlpc800.fit(data_train, target_train)
+
+    print("\n$ MLPClassifier: ")
+    print(mlpc100.score(data_test, target_test))
+    print(mlpc200.score(data_test, target_test))
+    print(mlpc300.score(data_test, target_test))
+    print(mlpc400.score(data_test, target_test))
+    print(mlpc500.score(data_test, target_test))
+    print(mlpc600.score(data_test, target_test))
+    print(mlpc700.score(data_test, target_test))
+    print(mlpc800.score(data_test, target_test))
+
+    # well, even worse ...
+    mlpr100 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(100,))
+    mlpr200 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(200,))
+    mlpr300 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(300,))
+    mlpr400 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(400,))
+    mlpr500 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(500,))
+    mlpr600 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(600,))
+    mlpr700 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(700,))
+    mlpr800 = MLPRegressor(solver='lbfgs', alpha=0.05, max_iter=9000, random_state=1, hidden_layer_sizes=(800,))
+
+    mlpr100.fit(data_train, target_train)
+    mlpr200.fit(data_train, target_train)
+    mlpr300.fit(data_train, target_train)
+    mlpr400.fit(data_train, target_train)
+    mlpr500.fit(data_train, target_train)
+    mlpr600.fit(data_train, target_train)
+    mlpr700.fit(data_train, target_train)
+    mlpr800.fit(data_train, target_train)
+
+    print("\n@ MLPRegressor: ")
+    print(mlpr100.score(data_test, target_test))
+    print(mlpr200.score(data_test, target_test))
+    print(mlpr300.score(data_test, target_test))
+    print(mlpr400.score(data_test, target_test))
+    print(mlpr500.score(data_test, target_test))
+    print(mlpr600.score(data_test, target_test))
+    print(mlpr700.score(data_test, target_test))
+    print(mlpr800.score(data_test, target_test))
+
+    # OutputCodeClassifier is also a decorator that decorates the 'estimator'
+    # The most important part is: user can define how many classes are there (code_size)
+
+    outputClass1 = OutputCodeClassifier(code_size=1, estimator=svm.SVC())
+    outputClass2 = OutputCodeClassifier(code_size=2, estimator=svm.SVC())
+    outputClass3 = OutputCodeClassifier(code_size=3, estimator=svm.SVC())
+    outputClass4 = OutputCodeClassifier(code_size=4, estimator=svm.SVC())
+    outputClass5 = OutputCodeClassifier(code_size=5, estimator=svm.SVC())
+    outputClass6 = OutputCodeClassifier(code_size=6, estimator=svm.SVC())
+    outputClass7 = OutputCodeClassifier(code_size=7, estimator=svm.SVC())
+
+    outputClass1.fit(data_train, target_train)
+    outputClass2.fit(data_train, target_train)
+    outputClass3.fit(data_train, target_train)
+    outputClass4.fit(data_train, target_train)
+    outputClass5.fit(data_train, target_train)
+    outputClass6.fit(data_train, target_train)
+    outputClass7.fit(data_train, target_train)
+
+    print("\n* OutputCodeClassifier: ")
+    print(outputClass1.score(data_test, target_test))
+    print(outputClass2.score(data_test, target_test))
+    print(outputClass3.score(data_test, target_test))
+    print(outputClass4.score(data_test, target_test))
+    print(outputClass5.score(data_test, target_test))
+    print(outputClass6.score(data_test, target_test))
+    print(outputClass7.score(data_test, target_test))
+
+    X, Y = make_multilabel_classification(n_samples=10, n_features=100, n_classes=3, random_state=1)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.7, random_state=1)
+
+    # What is the use of it... at least I know # of jobs doesn't affect results
+
+    mulOutputClass1 = MultiOutputClassifier(n_jobs=1, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass2 = MultiOutputClassifier(n_jobs=2, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass3 = MultiOutputClassifier(n_jobs=3, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass4 = MultiOutputClassifier(n_jobs=4, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass5 = MultiOutputClassifier(n_jobs=5, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass6 = MultiOutputClassifier(n_jobs=6, estimator=RandomForestClassifier(random_state=1))
+    mulOutputClass7 = MultiOutputClassifier(n_jobs=7, estimator=RandomForestClassifier(random_state=1))
+
+    mulOutputClass1.fit(X_train, Y_train)
+    mulOutputClass2.fit(X_train, Y_train)
+    mulOutputClass3.fit(X_train, Y_train)
+    mulOutputClass4.fit(X_train, Y_train)
+    mulOutputClass5.fit(X_train, Y_train)
+    mulOutputClass6.fit(X_train, Y_train)
+    mulOutputClass7.fit(X_train, Y_train)
+
+    print("\n+ MultiOutputClassifier: ")
+    print(mulOutputClass1.score(X_test, Y_test))
+    print(mulOutputClass2.score(X_test, Y_test))
+    print(mulOutputClass3.score(X_test, Y_test))
+    print(mulOutputClass4.score(X_test, Y_test))
+    print(mulOutputClass5.score(X_test, Y_test))
+    print(mulOutputClass6.score(X_test, Y_test))
+    print(mulOutputClass7.score(X_test, Y_test))
+
+    # Why the scores are always negative?
+
+    A, B = load_linnerud(return_X_y=True)
+    A_train, A_test, B_train, B_test = train_test_split(A, B, test_size=0.7)
+
+    mulOutputReg1 = MultiOutputRegressor(n_jobs=1, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg2 = MultiOutputRegressor(n_jobs=2, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg3 = MultiOutputRegressor(n_jobs=3, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg4 = MultiOutputRegressor(n_jobs=4, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg5 = MultiOutputRegressor(n_jobs=5, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg6 = MultiOutputRegressor(n_jobs=6, estimator=GradientBoostingRegressor(random_state=1))
+    mulOutputReg7 = MultiOutputRegressor(n_jobs=7, estimator=GradientBoostingRegressor(random_state=1))
+
+    mulOutputReg1.fit(A_train, B_train)
+    mulOutputReg2.fit(A_train, B_train)
+    mulOutputReg3.fit(A_train, B_train)
+    mulOutputReg4.fit(A_train, B_train)
+    mulOutputReg5.fit(A_train, B_train)
+    mulOutputReg6.fit(A_train, B_train)
+    mulOutputReg7.fit(A_train, B_train)
+
+    print("\n- MultiOutputRegressor: ")
+    print(mulOutputReg1.score(A_test, B_test))
+    print(mulOutputReg2.score(A_test, B_test))
+    print(mulOutputReg3.score(A_test, B_test))
+    print(mulOutputReg4.score(A_test, B_test))
+    print(mulOutputReg5.score(A_test, B_test))
+    print(mulOutputReg6.score(A_test, B_test))
+    print(mulOutputReg7.score(A_test, B_test))
---
+++ Practice/src/SklearnPractice/Others.py
@@ -0,0 +1,71 @@
+def otherPractice():
+    from sklearn.preprocessing import LabelBinarizer
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    import numpy as np
+
+    zero_to_two = np.arange(0, 3)
+    lb = LabelBinarizer()
+    print(zero_to_two)
+
+    # tmp = [0,1,2]
+    # [1, 0 ,0] means first item in tmp (a.k.a: 0)
+    # [0, 1 ,0] means second item in tmp (a.k.a: 1)
+    # [0, 0 ,1] means third item in tmp (a.k.a: 2)
+    print(lb.fit_transform(zero_to_two))
+
+    from sklearn.feature_selection import VarianceThreshold
+
+    X = [[1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1],
+         [0, 1, 1],
+         [0, 0, 1],
+         [0, 0, 0]]
+
+    sel = VarianceThreshold(threshold=(0.8 * (1 - 0.8)))  # remove all columns that are more than 80% same
+
+    print(sel.fit_transform(X))
+    
+    from sklearn.datasets import load_iris
+    from sklearn.feature_selection import SelectKBest, \
+        chi2  # chi2 is a math formula: https://www.google.com/search?q=chi2&oq=chi2&aqs=chrome..69i57j0i10i457j0l2j0i10j0j0i10j0i395l3.261j1j9&sourceid=chrome&ie=UTF-8#wptab=s:H4sIAAAAAAAAAONgVuLSz9U3MDItNitJfsToyi3w8sc9YSmbSWtOXmM04-IKzsgvd80rySypFNLgYoOy5Lj4pJC0aTBI8XAh8Xl2MUm6ppQmJ5Zk5ucl5jjn5yWnFpS45RflluYkLmKVSM7I1C0uLE0sSk1RKEktLlFIg0gBAOCLz8-NAAAA
+
+    X, Y = load_iris(return_X_y=True)
+
+    print(X)
+
+    kBest_1 = SelectKBest(chi2, k=1)
+    kBest_2 = SelectKBest(chi2, k=2)
+    kBest_3 = SelectKBest(chi2, k=3)
+    kBest_4 = SelectKBest(chi2, k=4)
+
+    print(kBest_1.fit_transform(X, Y))
+    print(kBest_2.fit_transform(X, Y))
+    print(kBest_3.fit_transform(X, Y))
+    print(kBest_4.fit_transform(X, Y))
+
+    from sklearn.feature_selection import SelectFromModel
+    from sklearn import svm
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1, train_size=0.7)
+
+    svc = svm.LinearSVC(max_iter=9000)
+
+    svc.fit(X_train, Y_train)
+    print("score of original: ", svc.score(X_test, Y_test))
+
+    X_new = SelectFromModel(svc, prefit=True).transform(X)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X_new, Y, random_state=1, train_size=0.7)
+
+    svc.fit(X_train, Y_train)
+    print("score of new: ", svc.score(X_test, Y_test))
+
+    print(X)
+    print(X_new)
