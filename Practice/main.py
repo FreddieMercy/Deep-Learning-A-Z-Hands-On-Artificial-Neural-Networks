c50bef8c45b3c785299b28ac78bfdf828d2f7b6e
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-02-11 23:56:12 -0800

    Vectorizer practices

--- Practice/main.py
+++ Practice/main.py
@@ -1,30 +1,31 @@
-from src.MatplotlibPractice import matplotlibPractice
-from src.NumpyPractice import numpyPractice
-from src.PandasPractice import pandasPractice
-from src.SklearnPractice.Imputation import imputationPractice
-from src.SklearnPractice.LinearRegression import linearRegressionPractice
-from src.SklearnPractice.Others import otherPractice
-from src.SklearnPractice.SVC import svcPractice
-from src.SklearnPractice.NearestNeighbors import nearestNeighborsPractice
-from src.SklearnPractice.DecisionTree import decisionTreePractice
-from src.SklearnPractice.Ensemble import ensemblePractice
-from Comparison import Comparison
+from sklearn.feature_extraction import text
+import pandas as pd
 
+corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document?',
+]
 
-def main():
-    numpyPractice()
-    matplotlibPractice()
-    pandasPractice()
-    linearRegressionPractice()
-    svcPractice()
-    nearestNeighborsPractice()
-    decisionTreePractice()
-    ensemblePractice()
-    otherPractice()
+doc = pd.read_csv("data/raw.txt", delimiter='\t')
 
+counter = text.CountVectorizer()
+counter.fit(corpus)
+names = counter.get_feature_names()
 
-main()
-print("--------------------------------------------------------------------")
-Comparison()
-
-imputationPractice()
+print(
+    counter.transform(
+        corpus).toarray())  # has four rows, same as 'corpus', and these numbers mean the # of occurance of the string in 'names' on that index
+print(names)
+print("-----------------------------------------------------------")
+vector = text.TfidfVectorizer()
+print(vector.fit_transform(doc))  # some texts may be filtered out b/c the algorithm thinks these are noises.
+# "I" will be filtered out because "Fredd'i'e" has 'i'
+print(vector.get_feature_names())
+print("-----------------------------------------------------------")
+transformer = text.TfidfTransformer()
+print(transformer.fit_transform(vector.fit_transform(doc)))
+print("-----------------------------------------------------------")
+counter2 = text.CountVectorizer()
+print(counter2.fit_transform(doc))  # see, same as 'transformer', which is TfidfVectorizer + TfidfTransformer
